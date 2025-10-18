3565c07730a14160ba4ba247ada1dffa8dd70a82
xclapsx-design <xclapsx@gmail.com>
2025-10-18 14:56:17 +0800

    Add src/App.js for React app container

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+// import MainComponent from "./2000_字_srs_測驗（tone配色・unicode修復版）";
+function App() {
+  return (
+    <div>
+      <h1>Pinyin Tone SRS</h1>
+      {/* <MainComponent /> */}
+      <p>Edit <code>src/App.js</code> to mount your main component.</p>
+    </div>
+  );
+}
+export default App;
